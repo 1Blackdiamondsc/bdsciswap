6f8e2a1a6ffb0222b5cc52553a64e948a39e56ef
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-10-02 08:03:32 -0400

    https://avatars.githubusercontent.com/u/73549208?s=96&v=4

---
+++ ATokenInterface.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.6.0;
+
+interface ATokenInterface {
+    function redeem(uint256 _amount) external;
+    function balanceOf(address _user) external view returns(uint256);
+    function principalBalanceOf(address _user) external view returns(uint256);
+}
